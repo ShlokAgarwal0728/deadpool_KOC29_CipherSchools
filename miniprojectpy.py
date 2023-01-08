f9b30c6db5d2509d18c2dbc5660bbfb8bab7a318
ShlokAgarwal0728 <117836084+ShlokAgarwal0728@users.noreply.github.com>
2023-01-08 15:05:13 +0530

    Update miniprojectpy.py

--- miniprojectpy.py
+++ miniprojectpy.py
@@ -6,3 +6,18 @@ day1  = date.today()             #CURRENT DATE
 day2 = date(2005, 1, 5)          #BIRTH DATE (yyyy,mm,dd)
 diff = day1 - day2
 print(diff.days)
+
+
+# #let's make a program to calculate how much days they lived on earth
+
+# #let's take users date of birth with year and month
+# day=int(input("write birth date ="))
+# month=int(input("write birth month ="))
+# year=int(input("write birth year ="))
+#  #let's convert users date of birth in datetime format
+# import datetime as dt
+
+# birth=dt.date(year,month,day)
+# today=dt.date.today()
+# delta=today-birth
+# print(delta)
